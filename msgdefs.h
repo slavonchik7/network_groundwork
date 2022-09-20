321ecf97cf971cbe4a7b0c5f287f08fc487ffa47
slavonchik7 <slavonchik0701@gmail.com>
2022-09-20 18:20:07 +0300

    modified msgdefs.h

--- msgdefs.h
+++ msgdefs.h
@@ -7,6 +7,11 @@
 #include <stdarg.h>
 
 
+#ifndef DEBUG_LOG
+#define DEBUG_LOG
+#endif /* DEBUG_LOG */
+
+#ifdef DEBUG_LOG
 #define LOG_PRINT(file, ...)  \
     do { \
             fprintf(file, "(FILE: %s, LINE: %d, func: %s) msg: \"", __FILE__, __LINE__, __func__); \
@@ -14,6 +19,9 @@
             fprintf(file, "\"\n"); \
             fflush(file); \
     } while (0);
+#else
+#define LOG_PRINT(file, ...)
+#endif /* DEBUG_LOG */
 
 #define LOG_ERR(...) LOG_PRINT(stderr, ##__VA_ARGS__)
 #define LOG_OUT(...) LOG_PRINT(stdout, ##__VA_ARGS__)
@@ -21,6 +29,7 @@
 
 
 
+#ifdef DEBUG_LOG
 #define LOG_PRINT_FRMT(file, frmt, ...)  \
     do { \
             fprintf(file, "(FILE: %s, LINE: %d, func: %s) msg: \""frmt"\"\n", \
@@ -28,6 +37,9 @@
                     __VA_ARGS__); \
             fflush(file); \
     } while (0);
+#else
+#define LOG_PRINT_FRMT(file, frmt, ...)
+#endif /* DEBUG_LOG */
 
 #define LOG_ERR_FRMT(frmt, ...) LOG_PRINT_FRMT(stderr, frmt, ##__VA_ARGS__)
 #define LOG_OUT_FRMT(frmt, ...) LOG_PRINT_FRMT(stdout, frmt, ##__VA_ARGS__)
